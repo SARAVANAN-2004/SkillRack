f042805216c05b113a55edc4e56131f6897a64c3
SARAVANAN-2004 <147891967+SARAVANAN-2004@users.noreply.github.com>
2024-12-19 19:48:22 +0530

    Update N_BishopsFillRemaining.java

--- src/set_1/N_BishopsFillRemaining.java
+++ src/set_1/N_BishopsFillRemaining.java
@@ -1,60 +1,60 @@
-package set_1;
-
 import java.util.Scanner;
 
-public class N_BishopsFillRemaining {
+public class NBishopFillingRows {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        byte board[][] = new byte[N][N];
-        boolean swDiagonals[] = new boolean[2 * N - 1];
-        boolean nwDiagonals[] = new boolean[2 * N - 1];
-        boolean bishopRows[] = new boolean[N];
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        byte[][] board = new byte[N][N];
+        boolean[] swd = new boolean[2 * N - 1];
+        boolean[] nwd = new boolean[2 * N - 1];
+        boolean[] bishop = new boolean[N];
 
         for (int row = 0; row < N; row++) {
             for (int col = 0; col < N; col++) {
-                board[row][col] = sc.nextByte();
+                board[row][col] = in.nextByte();
                 if (board[row][col] == 1) {
-                    bishopRows[row] = true;
+                    bishop[row] = true;
+                    nwd[col + row] = true;
+                    swd[col - row + N - 1] = true;
                 }
             }
         }
 
-        if (canPlace(N, 0, board, swDiagonals, nwDiagonals, bishopRows)) {
+        if (canPlace(N, 0, board, swd, nwd, bishop)) {
             for (int row = 0; row < N; row++) {
                 for (int col = 0; col < N; col++) {
                     System.out.print(board[row][col] + " ");
                 }
-                System.out.println("");
+                System.out.println();
             }
         } else {
             System.out.println("NotPossible");
         }
     }
 
-    private static boolean canPlace(int N, int row, byte[][] board, boolean[] swDiagonals, boolean[] nwDiagonals, boolean[] bishopRows) {
+    private static boolean canPlace(int N, int row, byte[][] board, boolean[] swd, boolean[] nwd, boolean[] bishop) {
         if (row == N) {
             return true;
         }
-        if (bishopRows[row]) {
-            return canPlace(N, row + 1, board, swDiagonals, nwDiagonals, bishopRows);
+        if (bishop[row]) {
+            return canPlace(N, row + 1, board, swd, nwd, bishop);
         }
         for (int col = 0; col < N; col++) {
-            if (!swDiagonals[col - row + N - 1] && !nwDiagonals[row + col]) {
+            if (!swd[col - row + N - 1] && !nwd[col + row]) {
                 board[row][col] = 1;
-                bishopRows[row] = true;
-                swDiagonals[col - row + N - 1] = true;
-                nwDiagonals[row + col] = true;
+                bishop[row] = true;
+                swd[col - row + N - 1] = true;
+                nwd[col + row] = true;
 
-                if (canPlace(N, row + 1, board, swDiagonals, nwDiagonals, bishopRows)) {
+                if (canPlace(N, row + 1, board, swd, nwd, bishop)) {
                     return true;
+                } else {
+                    board[row][col] = 0;
+                    bishop[row] = false;
+                    swd[col - row + N - 1] = false;
+                    nwd[col + row] = false;
                 }
-
-                board[row][col] = 0;
-                bishopRows[row] = false;
-                swDiagonals[col - row + N - 1] = false;
-                nwDiagonals[row + col] = false;
             }
         }
         return false;
